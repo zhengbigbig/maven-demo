93baca1097a91aea02a866e4217707d375002ffa
zhengbigbig <780357902@qq.com>
2019-06-27 10:42:11 +0800

    create synchronized

---
+++ src/main/java/threadsynchronized/Callme.java
@@ -0,0 +1,13 @@
+package threadsynchronized;
+
+public class Callme {
+    void call(String msg) {
+        System.out.println("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println("]");
+    }
+}
