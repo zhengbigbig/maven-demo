dea2e098f4458eb7c2271602b725dfb29a258745
zhengbigbig <780357902@qq.com>
2019-06-27 18:09:17 +0800

    多线程爬虫

--- src/main/java/com/company/spider/MultiThreadMain.java
+++ src/main/java/com/company/spider/MultiThreadMain.java
@@ -28,19 +28,24 @@ public class MultiThreadMain {
         String startUrl = "https://readhub.me/topic/5bMmlAm75lD";
         NewsWithRelated startNews = UrlNewsReader.read(startUrl);
 
-        searchSate.visit(startNews);
+        searchSate.visit(startNews); // 起始搜索点放到队列里
         while (searchSate.hasTarget()) {
             NewsWithRelated current = searchSate.poll();
+
+            // 把当前页面
             searchSate.addResult(current);
             List<SpiderThread> spiders = new ArrayList<SpiderThread>();
+
+            // 处理相关页面
             for (Map.Entry<String, String> entry : current.getRelateds().entrySet()) {
                 String url = entry.getValue();
+                // 创建线程去把页面爬下来，然后更新queue 和 visited
                 spiders.add(new SpiderThread(searchSate, url));
             }
 
-            //等待所有线程停止
+            //等待所有线程完成页面的读取
             for (SpiderThread spider : spiders) {
-                spider.join();
+                spider.join();  // 等待这个线程运行完成  使用join方法进行线程合并
             }
         }
         long endTime=System.currentTimeMillis();
--- src/main/java/com/company/spider/activiy/SpiderThread.java
+++ src/main/java/com/company/spider/activiy/SpiderThread.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import com.company.spider.model.*;
 
 public class SpiderThread extends Thread {
-
+    // 开启多线程去进行爬取
     private String url;
     private SearchSate searchSate;
 
@@ -18,8 +18,8 @@ public class SpiderThread extends Thread {
     @Override
     public void run() {
         try {
-            NewsWithRelated next = UrlNewsReader.read(url);
-            searchSate.visit(next);
+            NewsWithRelated next = UrlNewsReader.read(url);  // 涉及网络访问，耗时
+            searchSate.visit(next);  // 在访问后得到的扔到queue里面
         } catch (IOException e) {
             System.out.println("Ignored an error page: " + url);
         }
